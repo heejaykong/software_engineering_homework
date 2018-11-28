5ceada6c7b57930f14850a523e66c70fee08cf9a
Heejay Kong <heejaykong@naver.com>
2018-11-28 21:23:59 +0900

    Specified [Homework.java]

--- Homework/src/Homework.java
+++ Homework/src/Homework.java
@@ -1,36 +1,68 @@
-import java.awt.*;
-import javax.swing.*;
-//hello world
-public class Homework extends JFrame {
+import Contacts.*;
+import Appointments.*;
+// import TODO.*
 
-	private JButton [] menuBtn = new JButton [3];
-	
-	public Homework() {
-		setTitle("Menu");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300,300);
-		Container c = getContentPane();
-		GridLayout grid = new GridLayout(3,1,20,20);
-		c.setLayout(grid);
-		
-		menuBtn[0] = new JButton("楷遏贸");
-		menuBtn[0].setSize(200,40);
-		c.add(menuBtn[0]);
-		
-		menuBtn[1] = new JButton("距加 包府");
-		menuBtn[1].setSize(200,40);
-		c.add(menuBtn[1]);
-		
-		menuBtn[2] = new JButton("To-Do");
-		menuBtn[2].setSize(200,40);
-		c.add(menuBtn[2]);
-		
-		setVisible(true);
-	}
-	
-	public static void main(String[] args) {
-		new Homework();
-	}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+
+public class Homework extends JFrame {
+   
+   private Contacts contacts;
+   private Appointments appoints;
+   // private TODO
+   
+   private JButton [] menuBtn = new JButton [3];
+   private Font font;
+   
+   public Homework() {
+      setTitle("Menu");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setSize(500,500);
+      Container a = getContentPane();
+      GridLayout grid = new GridLayout(3,1,20,20);
+      a.setLayout(grid);
+      a.setBackground(Color.WHITE);
+      
+      font = new Font("蹈框", Font.BOLD, 30);
+      
+      menuBtn[0] = new JButton("楷遏贸");
+      menuBtn[0].setSize(200,40);
+      menuBtn[0].setFont(font);
+      a.add(menuBtn[0]);
+      
+      menuBtn[0].addActionListener(new ActionListener(){
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            contacts = new Contacts();   
+         }
+      });
+      
+      menuBtn[1] = new JButton("距加 包府");
+      menuBtn[1].setSize(200,40);
+      menuBtn[1].setFont(font);
+      a.add(menuBtn[1]);
+      
+      menuBtn[1].addActionListener(new ActionListener(){
+          @Override
+          public void actionPerformed(ActionEvent e) {
+             appoints = new Appointments();  
+          }
+       });
+      
+      menuBtn[2] = new JButton("To-Do");
+      menuBtn[2].setSize(200,40);
+      menuBtn[2].setFont(font);
+      a.add(menuBtn[2]);
+      
+      // 竣记府胶呈
+      
+      setVisible(true);
+   }
+   
+   public static void main(String[] args) {
+      new Homework();
+   }
 
 }
-
